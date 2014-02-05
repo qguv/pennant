7b079e866691dd0ecea80476d59cb0224ae8c77d
Quint Guvernator <quintus.public@gmail.com>
2014-02-05 15:40:18 -0500

    replace weird end-line characters

--- scrape.py
+++ scrape.py
@@ -3,12 +3,16 @@
 import requests
 
 payload = {
-        "term_code" :   "201420",   #Spring 2014
-        "term_subj" :   "0",        #ALL
-        "attr"      :   "0",        #ALL
-        "levl"      :   "0",        #ALL
-        "status"    :   "0",        #ALL
+    "term_code" :   "201420",   #Spring 2014
+    "term_subj" :   "0",        #ALL
+    "attr"      :   "0",        #ALL
+    "levl"      :   "0",        #ALL
+    "status"    :   "0",        #ALL
 }
 
 r = requests.post("https://courselist.wm.edu/courseinfo/searchresults", payload)
-print(r.text)
+
+with open("results.html", 'w') as f:
+    response = r.text.replace("", "")
+    f.write(r.text)
+
