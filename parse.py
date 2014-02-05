badb7417e601e46fbc4ca9fedefd4730b311afee
gwicks <gpwicks@email.wm.edu>
2014-02-05 16:59:03 -0500

    Get all courses

--- parse.py
+++ parse.py
@@ -7,7 +7,8 @@ with open("results.html","r") as f:
     text = f.read()
     
 section_pattern = re.compile(r'<td.*>\s*<a[^>]*>([^<]+)</a>\s*</td>\s*' + r'<td[^>]*>([^<]+)</td>\s*' * 11)
-result = re.search(section_pattern, text)
+results = re.findall(section_pattern, text)
 
-for i in range(1, 13):
-    print(result.group(i))
+for result in results:
+    for attr in result:
+        print(attr)
