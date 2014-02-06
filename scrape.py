b458c8d2b936e01882f8b3bbfa02599a36b97e47
Quint Guvernator <quintus.public@gmail.com>
2014-02-05 20:50:49 -0500

    prettied up scraper

--- scrape.py
+++ scrape.py
@@ -3,18 +3,19 @@
 
 import requests
 
+url = "https://courselist.wm.edu/courseinfo/searchresults"
+
 payload = {
-    "term_code" :   "201420",   #Spring 2014
-    "term_subj" :   "0",        #ALL
-    "attr"      :   "0",        #ALL
-    "levl"      :   "0",        #ALL
-    "status"    :   "0",        #ALL
+    "term_code": "201420",  # Spring 2014
+    "term_subj": "0",       # ALL
+    "attr": "0",            # ALL
+    "levl": "0",            # ALL
+    "status": "0",          # ALL
 }
 
-r = requests.post("https://courselist.wm.edu/courseinfo/searchresults", payload)
+r = requests.post(url, payload)
 response = r.text
 response = response.replace("\r", "\n")
 
 with open("results.html", 'w') as f:
     f.write(response)
-
