493f4c140f20b9f84d49ca2546adf840226c7dd6
Quint Guvernator <quintus.public@gmail.com>
2015-04-10 23:45:40 -0400

    out with django

--- pennant/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from pennant import views
-
-urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
-        url(r'^getlist', views.getlist, name='getlist'),
-        url(r'^jsonpcallback', views.jsonpcallback, name='jsonpcallback'),
-)
